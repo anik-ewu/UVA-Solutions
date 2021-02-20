65d6570616fd93fe4ad9bdb8293da1e397ebe929
anik <sabbirhasanik@gmail.com>
2021-02-21 02:58:51 +0600

    math

--- 11344 The Huge One.cpp
+++ 11344 The Huge One.cpp
@@ -8,7 +8,6 @@ const ll inf = 1e9;
 #define read(x)         freopen("in.txt","r",stdin);
 #define write(x)        freopen("out.txt","w",stdout);
 #define Fast            ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
-
 #define sz              3000005
 
 string s;
@@ -27,8 +26,8 @@ bool check(int d){
 
 int main(){
     Fast;///using Fast I/O
-    read(x);
-    write(x);
+    //read(x);
+    //write(x);
     int t, n,a;
     cin>>t;
     while(t--){
